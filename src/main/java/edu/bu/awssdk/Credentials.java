f58c9ed5595368e60a5eb120fb11535b25528d66
mukadder <mukadder@bu.edu>
2018-04-04 16:28:48 -0400

    ddd

--- src/main/java/edu/bu/awssdk/Credentials.java
+++ src/main/java/edu/bu/awssdk/Credentials.java
@@ -7,7 +7,7 @@ public class Credentials {
 
     public static final String access_key_id= "AKIAJARJRFIB2N3LFQDA";
 
-    public static final String secret_access_key ="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";
+    public static final String secret_access_key ="us/b+Pt+/myks4DVI8zEG";
 
 
 
